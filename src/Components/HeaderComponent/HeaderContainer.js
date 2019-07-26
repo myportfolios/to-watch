54951bfaf66892508643debb6884c023f4427875
Alexander <lexybiade@gmail.com>
2019-07-25 22:56:18 -0400

    saved changes before eslint configuration

---
+++ src/Components/HeaderComponent/HeaderContainer.js
@@ -0,0 +1,6 @@
+const getNames = () => {
+    return "name";
+
+
+    
+}
---
+++ src/Components/test.js
@@ -0,0 +1,10 @@
+// const echo = (string, number) => {
+//     for (var i = 0; i <= number; i++){
+//         console.log(string);
+//     }
+// }
+// echo("hello", 3);
+
+// app.listen(3000, () => {
+//     console.log("starting app");
+// })
